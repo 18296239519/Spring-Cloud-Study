428a5fc777e1bda085de76627292843db8ea4f59
赛尼奥尔·皮克 <50738710+18296239519@users.noreply.github.com>
2019-06-28 09:43:35 +0800

    Update Sp02ItemserviceApplication.java

--- sp02-itemservice/src/main/java/com/tedu/sp02/Sp02ItemserviceApplication.java
+++ sp02-itemservice/src/main/java/com/tedu/sp02/Sp02ItemserviceApplication.java
@@ -11,6 +11,7 @@ public class Sp02ItemserviceApplication {
 	//大萨达撒
 	//此时啊啊 
 	public static void main(String[] args) {
+		//这是王海涛
 		SpringApplication.run(Sp02ItemserviceApplication.class, args);
 	}
 
